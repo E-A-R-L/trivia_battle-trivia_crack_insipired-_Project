cad1e7dcbec938717ee4cba26e4ca849c0bb50fc
Earl <earl.dimarucot@gmail.com>
2026-03-29 15:49:18 -0700

    login and signup page layout

--- views/signup.php
+++ views/signup.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <head>
-<link rel="stylesheet" href="style.css">
-
+<link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
 <div id="title">Trivia Battle</div>
 
 <div id="login-box">
+    <h3>Welcome</h3>
     <label>Username</label><br>
     <input type="text" name="username"><br><br>
 
     <label>Password</label><br>
     <input type="password" name="password"><br><br>
 
-    <button>Sign Up</button>
-    <button>Login</button>
-</div>
+    <label>Email</label><br>
+    <input type="email" name="email"><br><br>
 
+    <label>Date of Birth</label><br>
+    <input type="date" name="dob"><br><br>
+
+    <button>Go Back</button>
+    <button>Sign Up</button>
+</div>
 
 </body>
 </html>
