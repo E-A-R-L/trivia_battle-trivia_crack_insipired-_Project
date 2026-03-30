1c3c6dbed29af1d211cccf5cefd8872ecf616fb5
Earl <earl.dimarucot@gmail.com>
2026-03-29 22:02:33 -0700

    menu implemented

---
+++ views/menu.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+<div id="title">Trivia Battle</div>
+<br><br>
+<div id="welcome">Welcome $name ready to test your trivia?</div>
+<br><br>
+
+<div id="menu-options">
+<button>Profile</button>
+<br>
+<button>Leaderboard</button>
+</div>
+<br>
+
+
+<div id="options">
+<button id="searchbtn">SEARCH</button>
+<button id="continuebtn">CONTINUE</button>
+<button id="challengebtn">CHALLENGE</button>
+</div>
+
+</body>
+</html>
--- views/signup.php
+++ views/signup.php
@@ -9,13 +9,16 @@
 <div id="login-box">
     <h3>Welcome</h3>
     <label>Username</label><br>
-    <input type="text" name="username"><br><br>
+    <input type="text" name="username">
+    <br><br>
 
     <label>Password</label><br>
-    <input type="password" name="password"><br><br>
+    <input type="password" name="password">
+    <br><br>
 
     <label>Email</label><br>
-    <input type="email" name="email"><br><br>
+    <input type="email" name="email">
+    <br><br>
 
     <label>Date of Birth</label><br>
     <input type="date" name="dob"><br><br>
