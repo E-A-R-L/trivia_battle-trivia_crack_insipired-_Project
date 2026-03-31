e558789e8b0cce7f7532676a3dbb0d8c31d685b0
Earl <earl.dimarucot@gmail.com>
2026-03-30 21:58:09 -0700

    leaderboard set

---
+++ views/leaderboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+<link rel="stylesheet" href="../style.css">
+</head>
+
+<body>
+
+<div id="title">Trivia Battle</div>
+<div id="top">Top Players</div>
+
+<div style="font-size:40px; margin-top:100px; margin-botton:0px">Leaderboard</div>
+
+<div id="leaderboard">
+
+    <div>     
+    <label>$Username</label>
+    <br><br>
+    <label>$wins</label>
+    <br><br>
+    </div>
+
+
+    <button>go back</button>
+</div>
+
+
+</body>
+</html>
